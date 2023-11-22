cfd7625de462707517b34d775723e731fbcd5baa
Mariia Vasileva <marysolodova1998@gmail.com>
2023-11-22 19:55:30 +0300

    fixed assets links

--- scripts/assets.js
+++ scripts/assets.js
@@ -1,11 +1,11 @@
 export function getCardLogo(cardType) {
     switch (cardType) {
         case 'mir':
-            return "/assets/icons/mir-logo.svg";
+            return "assets/icons/mir-logo.svg";
         case 'visa':
-            return '/assets/icons/visa-logo.svg';
+            return 'assets/icons/visa-logo.svg';
         case 'mc':
-            return '/assets/icons/mc-logo.svg';
+            return 'assets/icons/mc-logo.svg';
         case 'maestro':
             return 'assets/icons/maestro-logo.svg';
     }
